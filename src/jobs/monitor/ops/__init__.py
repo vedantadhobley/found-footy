e737c0e32069c24b184ad75172fff1f6d4cbee2b
vedantadhobley <vedanta.dhobley@gmail.com>
2025-11-22 20:33:01 +0000

    Update for naming and removal of deprecated logic.

--- src/jobs/monitor/ops/__init__.py
+++ src/jobs/monitor/ops/__init__.py
@@ -2,7 +2,7 @@
 from .activate_fixtures_op import activate_fixtures_op
 from .batch_fetch_active_op import batch_fetch_active_op
 from .detect_goal_delta_op import detect_goal_delta_op
-from .spawn_goal_jobs_op import trigger_goal_jobs_op
+from .trigger_goal_jobs_op import trigger_goal_jobs_op
 
 __all__ = [
     "activate_fixtures_op",
